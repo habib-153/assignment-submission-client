f09046649a08db913cfe917ee5578b16031ae0c8
habib-153 <h.r.sihab155@gmail.com>
2023-11-05 21:39:45 +0600

    all assignment added in assignment page

---
+++ src/Component/Assignments/A_Card.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+const A_Card = ({ assignment }) => {
+  const {
+    _id,
+    email,
+    assignment_name,
+    due_date,
+    assignment_image,
+    marks,
+    level,
+    description,
+  } = assignment;
+  return (
+    <div className="card p-4 flex flex-col md:flex-row items-center justify-between card-side bg-base-100 shadow-xl">
+      <figure className="w-[300px] h-[200px] rounded">
+        <img className="w-full h-full object-contain" src={assignment_image} alt="Movie" />
+      </figure>
+      <div className="card-body p-4 flex-1">
+        <h2 className="card-title">{assignment_name}</h2>
+        <p>{description}</p>
+        <div className="flex">
+          <p className='text-lg font-semibold text-[#3741c4]'>{level}</p>
+          <p>Mark: <span className="font-bold">{marks}</span></p>
+          <p className="font-semibold">Deadline: {due_date}</p>
+        </div>
+      </div>
+      <div className="card-actions">
+        <div className="flex items-center flex-row md:flex-col gap-4 md:space-y-4 mr-0">
+          <button className="btn btn-outline btn-ghost">View</button>
+          {/* <Link to={`updateCoffee/${_id}`}> */}
+          <button className="btn btn-outline btn-ghost">Edit</button>
+          {/* </Link> */}
+          <button className="btn btn-outline btn-warning">X</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default A_Card;
--- src/Component/Assignments/Assignments.jsx
+++ src/Component/Assignments/Assignments.jsx
@@ -1,8 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import A_Card from "./A_Card";
 
 const Assignments = () => {
+    const [assignments, setAssignments] = useState([])
+    const url = 'http://localhost:5000/assignments'
+
+    useEffect(()=>{
+        axios.get(url)
+        .then(res =>{
+          setAssignments(res.data)
+        })
+    },[])
     return (
         <div>
-            
+            <h2 className="mt-5 text-3xl md:text-5xl font-semibold text-center py-3 border-b-2">All Assignments</h2>
+            <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-base font-semibold">Level</span>
+                </label>
+                <select name="level" className="select select-bordered w-full text-lg">
+                  <option>Easy</option>
+                  <option>Medium</option>
+                  <option>Hard</option>
+                </select>
+            </div>
+            <div className="mt-4 grid grid-cols-1 gap-4">
+                {
+                    assignments.map(assignment => <A_Card 
+                    key={assignment._id} assignment={assignment}
+                    ></A_Card>)
+                }
+            </div>
         </div>
     );
 };
