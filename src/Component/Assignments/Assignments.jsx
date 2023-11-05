a9c7fd012491fdeae3dd668a233d87174364fa95
habib-153 <h.r.sihab155@gmail.com>
2023-11-05 23:32:17 +0600

    pagination in all assignment page added

--- src/Component/Assignments/Assignments.jsx
+++ src/Component/Assignments/Assignments.jsx
@@ -1,11 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import A_Card from "./A_Card";
+import './A.css'
 
 const Assignments = () => {
     const [assignments, setAssignments] = useState([])
     const [displayAssignments, setDisplayAssignments] = useState([]);
-    const url = 'http://localhost:5000/assignments'
+    const [count, setCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
+    const [itemsPerPage, setItemPerPage] = useState(5);
+
+    const numberOfPages = Math.ceil(count/itemsPerPage)
+    const pages = [...Array(numberOfPages).keys()]
+    const url = `http://localhost:5000/assignments?page=${currentPage}&size=${itemsPerPage}`
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/assignmentsCount')
+        .then(res => res.json())
+        .then(data=> setCount(data.count))
+    },[])
 
     useEffect(()=>{
         axios.get(url)
@@ -13,10 +26,10 @@ const Assignments = () => {
           setAssignments(res.data)
           setDisplayAssignments(res.data)
         })
-    },[])
+    },[currentPage, itemsPerPage, url])
     
     const handleAssignmentFilter = (level) => {
-        console.log(level)
+        //console.log(level)
         if(level === 'All'){
             setDisplayAssignments(assignments);
             console.log("all")
@@ -35,6 +48,24 @@ const Assignments = () => {
             setDisplayAssignments(hard);
         }
       }
+      const handleItemPerPage = e =>{
+        const value = parseInt(e.target.value)
+        console.log(value)
+        setItemPerPage(value)
+        setCurrentPage(0)
+    }
+
+    const handlePrev =()=>{
+        if (currentPage>0){
+            setCurrentPage(currentPage -1)
+        }
+    }
+
+    const handleNext =()=>{
+        if (currentPage < pages.length-1){
+            setCurrentPage(currentPage+1)
+        }
+    }
 
     return (
         <div>
@@ -62,6 +93,24 @@ const Assignments = () => {
                     key={assignment._id} assignment={assignment}
                     ></A_Card>)
                 }
+            </div>
+            <div className='pagination'>
+                <p>CurrentPage: {currentPage+1}</p>
+                <button onClick={handlePrev}>Prev</button>
+                {
+                    pages.map(page =><button 
+                        className={currentPage === page ? 'selected': undefined}
+                        onClick={()=>setCurrentPage(page)}
+                        key={page}
+                        >{page + 1}</button>)
+                }
+                <button onClick={handleNext}>Next</button>
+                <select className="bg-slate-300 p-2 rounded-lg" value={itemsPerPage} onChange={handleItemPerPage} name="" id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
