9c5646cfabc09f33f330ddbec146029325fcee3c
habib-153 <h.r.sihab155@gmail.com>
2023-11-06 18:08:51 +0600

    take assignment button functionality added

--- src/Component/Details/Details.jsx
+++ src/Component/Details/Details.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Details = () => {
   const assignment = useLoaderData();
@@ -13,6 +14,46 @@ const Details = () => {
     level,
     description,
   } = assignment;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, assignment_name, marks);
+
+    const form = e.target;
+    const assignment_pdf = form.assignment_pdf.value;
+    const note = form.note.value;
+    const submittedAssignment = {
+      assignment_name,
+      assignment_pdf,
+      email,
+      marks,
+      note,
+      assignment_image,
+    };
+
+    console.log(submittedAssignment);
+    fetch("http://localhost:5000/submittedAssignments", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(submittedAssignment),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success",
+            text: "Assignment Submitted successfully",
+            icon: "success",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Close",
+          });
+        }
+      });
+  };
+
   return (
     <div className="h-screen p-4 my-8  justify-between items-center mx-auto card-side">
       <div className="flex flex-col md:flex-row gap-4 items-center">
@@ -28,7 +69,54 @@ const Details = () => {
           </div>
           <p>{description}</p>
           <div className="w-full text-center">
-            <button className="btn btn-outline bg-[#1fe23c]  btn-ghost hover:bg-[#15b32c]">Take Assignment</button>
+            <button
+              onClick={() => document.getElementById("my_modal_3").showModal()}
+              className="btn btn-outline bg-[#1fe23c]  btn-ghost hover:bg-[#15b32c]"
+            >
+              Take Assignment
+            </button>
+            <dialog id="my_modal_3" className="modal">
+              <div className="modal-box">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                    ✕
+                  </button>
+                </form>
+                <form onSubmit={handleSubmit}>
+                  <h3 className="font-bold text-xl">Submit Your Assignment</h3>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text font-semibold">
+                        Assignment pdf Link
+                      </span>
+                    </label>
+                    <input
+                      type="url"
+                      name="assignment_pdf"
+                      placeholder="Enter Assignment pdf Link"
+                      className="input input-bordered"
+                      required
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text font-semibold">Note</span>
+                    </label>
+                    <textarea
+                      type="text"
+                      name="note"
+                      placeholder="Write a Quick Short note about the assignment if you have any."
+                      className="input h-20 input-bordered"
+                      required
+                    />
+                  </div>
+                  <button className="mt-4 btn btn-outline bg-[#1fe23c]  btn-ghost hover:bg-[#15b32c]">
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </dialog>
           </div>
         </div>
       </div>
